a1bcdea8f340093c73fc815e1f1237fa0b4faddb
Vladan <vgloncak@gmail.com>
2017-07-30 20:24:46 +0200

    Drobné opravy prvej úlohy.

--- assignment3_2.py
+++ assignment3_2.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 import sys
-import collections as col
-# import icu
 import math
 import random
 import datetime
@@ -10,12 +8,13 @@ import itertools
 from optparse import OptionParser
 from sys import stdin as sin
 from sys import stdout as sout
+from collections import Counter
 
 STARTw = "<s>"
 STARTt = "<s>"
 
 
-def getprob(p, h):
+def get_prob(p, h):
     """ Returns a corresponding value from the given tuple if an item is in the tuple, otherwise returns zero."""
     if h in p:
         return p[h]
@@ -26,23 +25,25 @@ def getprob(p, h):
 def get_parametres_superv(data_wt):
     count = len(data_wt)
     tags = [t for (_, t) in data_wt]
-    tags_uniq = col.Counter(tags)
-    data_uniq = col.Counter([(w, t) for (w, t) in data_wt])
-    p_wt = {(w, t): data_uniq[w, t] / tags_uniq[t] for (w, t) in
-            data_uniq}  # todo: v slidech je psano v jinem poradi (tag,w), je to opravdu totez??????
+    tags_uniq = Counter(tags)
+    data_uniq = Counter(data_wt)  # Counter([(w, t) for (w, t) in data_wt])
+    p_wt = {(w, t): data_uniq[w, t] / tags_uniq[t] for (w, t) in data_uniq}
+    # todo: v slidech je psano v jinem poradi (tag,w), je to opravdu totez??????
+    # @ add todo: naopak je to len preto, lebo P(vugenerujem w | viem t), ale v dictionary je to jedno, kedze hladas tu pravdepodobnost ale neviem naisto
     # p_wt=p_wt.union({(w,t):0 for w in {w for (w,_) in data_wt} for t in tags_uniq if (w,t) not in data_uniq })
-    pc = [{(w, t): data_uniq[w, t] for (w, t) in data_uniq},
-          tags_uniq]  # todo: v slidech je psano v jinem poradi (tag,w), je to opravdu totez??????
+    pc = [{(w, t): data_uniq[w, t] for (w, t) in data_uniq}, tags_uniq]
+    # constant probability mass (avoiding zeros)
     p_t0 = 1 / len(tags_uniq)
+    # unigram probabilities
     p_t1 = {t: tags_uniq[t] / count for t in tags_uniq}
-    bigram_tags = col.Counter(zip(tags[:-1], tags[1:]))
+    # bigram probabilities
+    bigram_tags = Counter(zip(tags[:-1], tags[1:]))
     p_t2 = {(t1, t2): (bigram_tags[t1, t2] / tags_uniq[t1]) for (t1, t2) in bigram_tags}
-
-    trigram_tags = col.Counter([t for t in zip(tags[:-2], tags[1:-1], tags[2:])])
+    # trigram probabilities
+    trigram_tags = Counter([trig for trig in zip(tags[:-2], tags[1:-1], tags[2:])])
     p_t3 = {(t1, t2, t3): (trigram_tags[t1, t2, t3] / bigram_tags[t1, t2]) for (t1, t2, t3) in trigram_tags}
-
     p_tt = [p_t0, p_t1, p_t2, p_t3]
-    return (p_wt, p_tt, pc)
+    return p_wt, p_tt, pc
 
 
 # ----------------- smoothing EM algorithm------------------------------------------
@@ -51,16 +52,17 @@ def EMiter(data, p, l):
     """An one iteration of EM algorithm."""
     tri = [u for u in zip(data[:-2], data[1:-1], data[2:])]
     pp = {
-        (i, j, k): l[3] * getprob(p[3], (i, j, k)) + l[2] * getprob(p[2], (j, k)) + l[1] * getprob(p[1], k) + l[0] * p[
-            0]
+        (i, j, k): l[3] * get_prob(p[3], (i, j, k)) + l[2] * get_prob(p[2], (j, k)) + l[1] * get_prob(p[1], k) + l[0] *
+                                                                                                                 p[
+                                                                                                                     0]
         for (i, j, k) in set(tri)}  # new p'(lambda)
     c = [0, 0, 0, 0]
     for (i, j, k) in tri:
         pptemp = pp[(i, j, k)]
         c[0] = c[0] + l[0] * p[0] / pptemp
-        c[1] += l[1] * getprob(p[1], k) / pptemp
-        c[2] += l[2] * getprob(p[2], (j, k)) / pptemp
-        c[3] += l[3] * getprob(p[3], (i, j, k)) / pptemp
+        c[1] += l[1] * get_prob(p[1], k) / pptemp
+        c[2] += l[2] * get_prob(p[2], (j, k)) / pptemp
+        c[3] += l[3] * get_prob(p[3], (i, j, k)) / pptemp
     return [i / sum(c) for i in c]  # normalised
 
 
@@ -87,11 +89,11 @@ def smoothEM(p, heldout, traindata):
     l = EMalgorithm(heldout, p)  # get lambdas
     tri = [i for i in zip(traindata[:-2], traindata[1:-1], traindata[2:])]
     # TODO: TO JE DOCELA DIVNE, ZE TU POUZIVAM TRIGRAMY MISTO VSECH MOZNYCH TROJIC (pozor, stejne tak v EMiter) , tady to nedává smysl, TODO: OVERIT, ZE MAM ZDE POUZIT TRAINING DATA
-    ttrainset=set(traindata) #todo:pouzit ta globalni, pokud takto spravne
+    ttrainset = set(traindata)  # todo:pouzit ta globalni, pokud takto spravne
     pt_em = {
         (i, j, k): (
-            l[0] * p[0] + l[1] * getprob(p[1], k) + l[2] * getprob(p[2], (j, k)) + l[3] * getprob(p[3], (i, j, k)))
-        #for (i, j, k) in tri}
+            l[0] * p[0] + l[1] * get_prob(p[1], k) + l[2] * get_prob(p[2], (j, k)) + l[3] * get_prob(p[3], (i, j, k)))
+        # for (i, j, k) in tri}
         for i in ttrainset for j in ttrainset for k in ttrainset}
     return pt_em
 
@@ -100,7 +102,8 @@ def smoothAdd1(pc, data, tagset):
     """
     Do smoothing by Adding 1, need counts of c(t,w) and c(h) from train data and need test data for smooting.
     """
-    pwt = {(w, t): ((getprob(pc[0], (w, t)) + 1) / (getprob(pc[1], t) + len(wordset) * len(tagset))) for w in words for
+    pwt = {(w, t): ((get_prob(pc[0], (w, t)) + 1) / (get_prob(pc[1], t) + len(wordset) * len(tagset))) for w in words
+           for
            t in tagset}
     return pwt
 
@@ -121,12 +124,12 @@ class Pwt:
         """
         Returns smoothed p(w|t). Suppose that w and t are from known wordset and tagset, not unknown.
         """
-        #print("DEBUG: in get_pwt: w: ",w,", t:",t)
-        #print(getprob(self.wt_bigram_counts, (w, t)) + 1, " ", getprob(self.t_unigram_counts, t), " ", self.len_wordset,
-         #     " ", self.len_tagset)
-        if isOOV: return 1/self.len_tagset # if the w is out-of-vocabulary, then use uniform distribution
-        return ((getprob(self.wt_bigram_counts, (w, t)) + 1) / (
-            getprob(self.t_unigram_counts, t) + self.len_wordset * self.len_tagset))
+        # print("DEBUG: in get_pwt: w: ",w,", t:",t)
+        # print(getprob(self.wt_bigram_counts, (w, t)) + 1, " ", getprob(self.t_unigram_counts, t), " ", self.len_wordset,
+        #     " ", self.len_tagset)
+        if isOOV: return 1 / self.len_tagset  # if the w is out-of-vocabulary, then use uniform distribution
+        return ((get_prob(self.wt_bigram_counts, (w, t)) + 1) / (
+            get_prob(self.t_unigram_counts, t) + self.len_wordset * self.len_tagset))
 
 
 class Ptt:
@@ -146,66 +149,65 @@ class Ptt:
         return self.p_t[t3, t1, t2]  # TODO: Možná udělat časem dynamicky
 
 
+# def getword(word,)
+def viterbi(text, tagset, wordset, Pwt, Ptt):
+    if len(text) == 0: return []
+    isOOV = False  # indicates if proceeded word is out-of-vocabulary
+    tagsetcontainsSTART = False
+    while STARTt in tagset:
+        tagset.remove(STARTt)
+        tagsetcontainsSTART = True
+        # we dont want to have this tag in the middle of the tag sequence,
+        # for sure while instead of only remove
+    if text[0] != STARTw or text[1] != STARTw:
+        warnings.warn("inconsistend data, start tokens", Warning)
 
-#def getword(word,)
-def viterbi(text,tagset,wordset,Pwt,Ptt):
+    V = {}  # structure for remember viterbi computation
+    # V[time][state]=(probability,path) where state==(tag1,tag2)
+    path = {}  # the best path to some state
+    V[0] = {}
+    V[1] = {}
+    # --- initialisation, starting state
+    # V[1][STARTt,STARTt]=(1,[(STARTw,STARTt),(STARTw,STARTt)])
+    V[1][STARTt, STARTt] = (0, [(STARTw, STARTt), (STARTw, STARTt)])
+    # --- finding the best way
+    for k in range(2, len(text)):
+        isOOV = False
+        w = text[k]
+        now = k % 2  # k modulo 2 instead of k; it is sufficient to remember
+        prev = (now + 1) % 2  # instead of k-1;          only actual and previous time
+        V[now] = {}
+        if w not in wordset:
+            print("out-of-vocabulary: ", w)
+            isOOV = True
+        for t in tagset:
+            bests = {}
+            bestpath = []
+            # maxprob=0
+            maxprob = -float('inf')
+            for (i, j) in V[prev]:
+                # value=V[prev][i,j][0]*Ptt.get_ptt(t,i,j)
+                value = V[prev][i, j][0] + math.log(Ptt.get_ptt(t, i, j), 2)
+                # if value==0: Chtělo by to aspoň takovýto treshold, zahodit všechny stavy mající value=0, což může být velmi velmi malé číslo zaokrouhlené na 0
+                if value >= maxprob:  # '=' because of very small numbers
+                    bests[0] = i
+                    bests[1] = j
+                    maxprob = value
+                    bestpath = V[prev][i, j][1]
+            V[now][bests[1], t] = (maxprob + math.log(Pwt.get_pwt(w, t, isOOV), 2), bestpath + [(w, t)])
+            # V[now][bests[1],t]=(maxprob*Pwt.get_pwt(w,t,isOOV),bestpath+[(w,t)])
+    if tagsetcontainsSTART: tagset.add(STARTt)  # to be the same as at start
+    # --- final search the best tag sequence
+    maxprob = -float('inf')
+    # maxprob=0
+    ends = {}  # the best end state
+    for s in V[now]:
+        if V[now][s][0] >= maxprob:
+            maxprob = V[now][s][0]
+            ends = s
+            # if(maxprob==0): warnings.warn("final sequence probability is zero", Warning)
+    return V[now][ends][1]
 
-        if len(text)==0: return []
-        isOOV=False # indicates if proceeded word is out-of-vocabulary
-        tagsetcontainsSTART=False
-        while STARTt in tagset: 
-            tagset.remove(STARTt)
-            tagsetcontainsSTART=True 
-            # we dont want to have this tag in the middle of the tag sequence,  
-            # for sure while instead of only remove
-        if text[0]!=STARTw or text[1]!=STARTw:
-            warnings.warn("inconsistend data, start tokens",Warning)
-       
-        V={}    # structure for remember viterbi computation
-                # V[time][state]=(probability,path) where state==(tag1,tag2)
-        path={} # the best path to some state
-        V[0]={} 
-        V[1]={}
-        # --- initialisation, starting state
-       # V[1][STARTt,STARTt]=(1,[(STARTw,STARTt),(STARTw,STARTt)])
-        V[1][STARTt,STARTt]=(0,[(STARTw,STARTt),(STARTw,STARTt)])
-        # --- finding the best way
-        for k in range(2,len(text)):
-                isOOV=False
-                w=text[k]
-                now=k%2        # k modulo 2 instead of k; it is sufficient to remember 
-                prev=(now+1)%2 # instead of k-1;          only actual and previous time
-                V[now]={}
-                if w not in wordset:
-                    print("out-of-vocabulary: ",w) 
-                    isOOV=True
-                for t in tagset:
-                    bests={}
-                    bestpath=[]
-                   # maxprob=0
-                    maxprob=-float('inf')
-                    for (i,j) in V[prev]:
-                       # value=V[prev][i,j][0]*Ptt.get_ptt(t,i,j)
-                        value=V[prev][i,j][0]+math.log(Ptt.get_ptt(t,i,j),2)
-                       # if value==0: Chtělo by to aspoň takovýto treshold, zahodit všechny stavy mající value=0, což může být velmi velmi malé číslo zaokrouhlené na 0
-                        if value>=maxprob: # '=' because of very small numbers  
-                            bests[0]=i
-                            bests[1]=j
-                            maxprob=value
-                            bestpath=V[prev][i,j][1]
-                    V[now][bests[1],t]=(maxprob+math.log(Pwt.get_pwt(w,t,isOOV),2),bestpath+[(w,t)])
-                   # V[now][bests[1],t]=(maxprob*Pwt.get_pwt(w,t,isOOV),bestpath+[(w,t)])
-        if tagsetcontainsSTART: tagset.add(STARTt)  # to be the same as at start
-        # --- final search the best tag sequence
-        maxprob=-float('inf')
-        #maxprob=0
-        ends={}              # the best end state
-        for s in V[now]:
-                if V[now][s][0]>=maxprob:
-                    maxprob=V[now][s][0]
-                    ends=s
-                #if(maxprob==0): warnings.warn("final sequence probability is zero", Warning)
-        return V[now][ends][1]
 
 # -----------------------------initialization-------------------------------
 
@@ -219,16 +221,20 @@ parser.add_option("-u", "--unsupervised",
                   help="Use unsupervised method (the default option)")
 (options, args) = parser.parse_args()
 file_name = args[0]
-f = open(file_name, encoding="iso-8859-2", mode='rt')
+file = open(file_name, encoding="iso-8859-2", mode='rt')
 supervised = options.supervised
 
 # ------ data preparation ---------
 
-data = [l.split('/', 1) for l in f.read().splitlines()]  # items in format: word,speech-tag which can contains '/'
-dataT = [(STARTw, STARTt), (STARTw, STARTt)] + data[:60000]
-dataH = [(STARTw, STARTt), (STARTw, STARTt)] + data[-60000:-40000]
-dataS = [(STARTw, STARTt), (STARTw, STARTt)] + data[-40000:] # the right testing data
-#dataS = [(STARTw, STARTt), (STARTw, STARTt)] + data[-39:] # testingdata for debuging
+data = []
+# @ prerobil som to aby tam boli rovno tuple a bolo to potom jednoduchsie
+for line in file:
+    w, t = line.strip().split(sep='/', maxsplit=1)
+    data.append((w, t))
+# @ tu bola chyba, trenovacie data mali iba 60000 vzorov, ale ich ma byt ten zvysok --- pridane minusko
+dataT = [(STARTw, STARTt), (STARTw, STARTt)] + data[:-60000]  # training data
+dataH = [(STARTw, STARTt), (STARTw, STARTt)] + data[-60000:-40000]  # held_out data used for smoothing
+dataS = [(STARTw, STARTt), (STARTw, STARTt)] + data[-40000:]  # testing data
 data = []  # for gc
 
 tagsetT = set([t for (_, t) in dataT])
@@ -242,7 +248,7 @@ if supervised:
 else:
     print("todo")
     sys.exit()
-#p_t = smoothEM(pp[1], [t for (_, t) in dataH], [t for (_, t) in dataT])  # probabilities p_t1,p_t2,p_t3
+# p_t = smoothEM(pp[1], [t for (_, t) in dataH], [t for (_, t) in dataT])  # probabilities p_t1,p_t2,p_t3
 # todo: toto neni sikovne, zbytecne zabira moc pameti, lepsi neco typu getter a vzdy spocist
 # p_wt=smoothAdd1(pp[2],[w for (w,_) in dataT],set([t for (_,t) in dataT]))
 # #sem by šlo dát i dataH, resp. cele p_wt spocitat i z heldout - zabiralo hodne pameti
@@ -250,6 +256,6 @@ pwt = Pwt(pp[2][0], pp[2][1], len(wordsetT), len(tagsetT))
 pt = Ptt(pp[1], [t for (_, t) in dataH], [t for (_, t) in dataT])
 # viterbi(dataS,tagsetT, wordsetT) # zvlážit, zda nedat tagset a wordset i z heldout
 # potřebuji p(t|u,v), p_wt(w/t) = c_wt(t,w)/c_t(t)
-tagged=viterbi([w for (w,_) in dataS],tagsetT,wordsetT,pwt,pt) #TODO: nespoustet na cely text, ale pouze na casti
+tagged = viterbi([w for (w, _) in dataS], tagsetT, wordsetT, pwt,
+                 pt)  # TODO: nespoustet na cely text, ale pouze na casti
 for p in tagged: print(p)
-
