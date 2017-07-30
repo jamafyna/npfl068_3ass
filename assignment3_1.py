bdc81223777b1fd251e9b525b2c87063a2d99cf2
Vladan <vgloncak@gmail.com>
2017-07-30 13:58:14 +0200

    Prvá úloha prerobená na delenie viet

--- assignment3_1.py
+++ assignment3_1.py
@@ -6,12 +6,29 @@ from nltk.tag import brill, brill_trainer, UnigramTagger
 
 
 def train_brill_tagger(train_data, **kwargs):
+    """Trains the Brill's tagger on the given data with the initial rules from the demo and initial unigram tagger"""
     # use the initial rules from the NLTK demonstration
     templates = brill.nltkdemo18()
     # use unigram tagger as the initial tagger
     initial_tagger = UnigramTagger(train_data)
     trainer = brill_trainer.BrillTaggerTrainer(initial_tagger, templates, deterministic=True)
     return trainer.train(train_data, **kwargs)
+
+
+def split_sentences(data_to_split):
+    """Splits the data into sentences and returns a list of lists of word-tag tuples"""
+    sentences = []
+    sentence = []
+    for word, tag in data_to_split:
+        if word != '###':
+            sentence.append((word, tag))
+        else:
+            if sentence:
+                sentences.append(sentence)
+            sentence = []
+    if sentence:
+        sentences.append(sentence)
+    return sentences
 
 
 # parse command lines arguments
@@ -27,36 +44,46 @@ for line in file:
 
 # the initial split from the assignment
 testing = data[-40000:]
+testing_sentences = split_sentences(testing)
 training = data[:-60000]
+training_sentences = split_sentences(training)
 print('The accuracy obtained by the 1st split is:')
-tagger = train_brill_tagger([training])
-print(tagger.evaluate([testing]))
+tagger = train_brill_tagger(training_sentences)
+print(tagger.evaluate(testing_sentences))
 
 # the second split from the assignment
 # held-out is 40000-60000
 testing = data[:40000]
+testing_sentences = split_sentences(testing)
 training = data[60000:]
+training_sentences = split_sentences(training)
 print('The accuracy obtained by the 2nd split is:')
-tagger = train_brill_tagger([training])
-print(tagger.evaluate([testing]))
+tagger = train_brill_tagger(training_sentences)
+print(tagger.evaluate(testing_sentences))
 
 # held-out is 60000-80000
 testing = data[80000:120000]
+testing_sentences = split_sentences(testing)
 training = data[:6000] + data[120000:]
+training_sentences = split_sentences(training)
 print('The accuracy obtained by the 3rd split is:')
-tagger = train_brill_tagger([training])
-print(tagger.evaluate([testing]))
+tagger = train_brill_tagger(training_sentences)
+print(tagger.evaluate(testing_sentences))
 
 # held-out is 160000-180000
 testing = data[120000:160000]
+testing_sentences = split_sentences(testing)
 training = data[:120000] + data[180000:]
+training_sentences = split_sentences(training)
 print('The accuracy obtained by the 4th split is:')
-tagger = train_brill_tagger([training])
-print(tagger.evaluate([testing]))
+tagger = train_brill_tagger(training_sentences)
+print(tagger.evaluate(testing_sentences))
 
 # use the held outs from second and third as the training data
 testing = data[40000:80000]
+testing_sentences = split_sentences(testing)
 training = data[:40000] + data[100000:]
+training_sentences = split_sentences(training)
 print('The accuracy obtained by the 5th split is:')
-tagger = train_brill_tagger([training])
-print(tagger.evaluate([testing]))
+tagger = train_brill_tagger(training_sentences)
+print(tagger.evaluate(testing_sentences))
