63e5ef18089a47ad0774222de866f56f1dadce48
Vladan <vgloncak@gmail.com>
2017-07-31 17:41:48 +0200

    EM, zostava vyhladit rozdelenie, asi tam budu bugy

--- functions.py
+++ functions.py
@@ -27,93 +27,208 @@ def get_distributions(data_wt):
     return p_output, (p0, p1, p2)
 
 
-def compute_forward_probabilities(transition_matrix, emission_matrix, sentence, n):
-    k = sentence.size
-    alpha = np.zeros((n, k + 1))
-    # forward computation
-    alpha[:, 0] = 1.0 / n
-    for obs_ind in range(k):
+def preprocess_data(my_data, word_to_index):
+    """Splits the data into sentences, strips off the tags and converts them to indices"""
+    processed_data = []
+    s = []
+    for my_word, tag in my_data:
+        # the first token is not really an observation
+        if my_word != '###':
+            s.append(word_to_index[my_word])
+        else:
+            if s:
+                processed_data.append(s)
+            s = []
+    if s:
+        processed_data.append(s)
+    return processed_data
+
+
+def compute_forward_probabilities(t_matrix, e_matrix, sentence, n):
+    """Computes the forward probability masses"""
+    l = len(sentence)
+    alpha_m = np.zeros((n, l + 1))
+    # the starting state is 0
+    # at the beginning, just the probability distribution of states follow by the starting state
+    alpha_m[:, 0] = t_matrix[0, :]
+    # for all the observations -- words in the sentence
+    for ii in range(l):
         # we select the "column" in the complete trellis
-        trellis_column = np.matrix(alpha[:, obs_ind])
-        alpha[:, obs_ind + 1] = trellis_column * np.matrix(transition_matrix) * \
-                                np.matrix(np.diag(emission_matrix[:, sentence[obs_ind]]))
-    return alpha
+        trellis_c = np.matrix(alpha_m[:, ii])
+        #      Alpha(s',i) = alpha(s,i-1) * p(s'|s) * p(y_i|s)
+        alpha_m[:, ii + 1] = trellis_c * t_matrix * np.diag(e_matrix[:, sentence[ii]])
+        # normalization after each stage (slide 185)
+        alpha_m[:, ii + 1] /= np.sum(alpha_m[:, ii + 1])
+    return alpha_m
 
 
-def compute_backward_probabilities(transition_matrix, emission_matrix, sentence, n):
-    k = sentence.size
-    beta = np.zeros((n, k + 1))
-    # backward computation
-    beta[:, -1] = 1.0
-    for obs_ind in range(k, 0, -1):
-        trellis_column = np.matrix(beta[:, obs_ind]).transpose()
-        beta[:, obs_ind - 1] = (np.matrix(transition_matrix) * \
-                                np.matrix(np.diag(emission_matrix[:, sentence[obs_ind - 1]])) * \
-                                trellis_column).transpose()
-    return beta
+def compute_backward_probabilities(t_matrix, e_matrix, sentence, n):
+    """Computes the backward probability masses"""
+    l = len(sentence)
+    beta_m = np.zeros((n, l + 1))
+    beta_m[:, -1] = 1.0
+    for ii in range(l, 0, -1):
+        trellis_c = np.matrix(beta_m[:, ii]).transpose()
+        beta_m[:, ii - 1] = np.transpose(t_matrix * np.diag(e_matrix[:, sentence[ii - 1]]) * trellis_c)
+        # normalization
+        beta_m[:, ii - 1] /= np.sum(beta_m[:, ii - 1])
+    return beta_m
 
 
-def collect_counts():
-    pass
+def baum_welch(num_states, num_obs, observ, epsilon=.00001):
+    # allocate
+    A_mat = np.ones((num_states, num_states))
+    A_mat = A_mat / np.sum(A_mat, 1)
+    O_mat = np.ones((num_states, num_obs))
+    O_mat = O_mat / np.sum(O_mat, 1)
+    theta = np.zeros((num_states, num_states, observ.size))
+    while True:
+        old_A = A_mat
+        old_O = O_mat
+        A_mat = np.ones((num_states, num_states))
+        O_mat = np.ones((num_states, num_obs))
+        # expectation step, forward and backward probs
+        P, alfa, beta = fb_alg(old_A, old_O, observ)
+        # need to get transitional probabilities at each time step too
+        for a_ind in range(num_states):
+            for b_ind in range(num_states):
+                for t_ind in range(observ.size):
+                    theta[a_ind, b_ind, t_ind] = \
+                        alfa[a_ind, t_ind] * \
+                        beta[b_ind, t_ind + 1] * \
+                        old_A[a_ind, b_ind] * \
+                        old_O[b_ind, observ[t_ind]]
+        # form A_mat and O_mat
+        for a_ind in range(num_states):
+            for b_ind in range(num_states):
+                A_mat[a_ind, b_ind] = np.sum(theta[a_ind, b_ind, :]) / np.sum(P[a_ind, :])
+        A_mat = A_mat / np.sum(A_mat, 1)
+        for a_ind in xrange(num_states):
+            for o_ind in xrange(num_obs):
+                right_obs_ind = np.array(np.where(observ == o_ind)) + 1
+                O_mat[a_ind, o_ind] = np.sum(P[a_ind, right_obs_ind]) / np.sum(P[a_ind, 1:])
+        O_mat = O_mat / np.sum(O_mat, 1)
+        # compare
+        if np.linalg.norm(old_A - A_mat) < .00001 and np.linalg.norm(old_O - O_mat) < .00001:
+            break
+    # get out
+    return A_mat, O_mat
 
 
-def reestimate():
-    pass
+def collect_counts_and_reestimate(alfa, beta, t_matrix, e_matrix, sentence, n):
+    l = len(sentence)
+    inc = np.zeros((n, n, l))
+    for ii in range(n):
+        for jj in range(n):
+            for k in range(l):
+                # inc(y,s,s')  = alfa(s,i)   * p(s'|s)          * p(y|s')                   * beta(s'|i+1)
+                # from slide 189, in Wikipedia it is denoted with ksi
+                inc[ii, jj, k] = alfa[ii, k] * t_matrix[ii, jj] * e_matrix[jj, sentence[k]] * beta[jj, k + 1]
+
+    t_matrix_new = np.zeros(t_matrix.size)
+    # probability that we are in state state s in time i -- gama[s,i]
+    # see https://en.wikipedia.org/wiki/Baum–Welch_algorithm
+    gamma = np.multiply(alfa, beta)
+    # normalize each trellis stage i.e. normalize columns -- divide each column by its sum
+    gamma /= np.sum(gamma, 0)
+    # compute
+    for ii in range(n):
+        for jj in range(n):
+            # transition probability = sum inc over time / sum gamma over time
+            # to get transition s -> s' independent of time, supposing that we are in s
+            t_matrix_new[ii, jj] = np.sum(inc[ii, jj, :]) / np.sum(gamma[ii, :])
+    t_matrix_new /= np.sum(t_matrix_new, 1)
+
+    # get the effective vocabulary size
+    vocab_size = e_matrix.shape[1]
+    e_matrix_new = np.zeros(e_matrix.shape)
+    # for every state
+    for ii in range(n):
+        # and every known (and the special OOV word)
+        for word in range(vocab_size):
+            # get all the positions where the word is in this sentence
+            indices = [i + 1 for i, x in enumerate(sentence) if x == word]
+            # estimate the emission probability of s emitting y as
+            # probability of state s emitting y in any time (sum over times)
+            # over total probability of being in that state at any time (at the beginning we are in imaginary state 0)
+            e_matrix_new[ii, word] = np.sum(gamma[ii, indices]) / np.sum(gamma[ii, 1:])
+    # normalize rows, because they represent emittion distribution of the state
+    e_matrix_new /= np.sum(e_matrix_new, 1)
+    # compare
+    return t_matrix_new, e_matrix_new
 
 
-# def baum_welch(training_data, held_out_data):
-#     # initialization
-#     whole_data = training_data
+def baum_welch(training_data, held_out_data, epsilon=0.001):
+    # initialization
+    whole_data = training_data
 
-file = open('data/texten2.ptg', 'rt')
-data = []
-for line in file:
-    w, t = line.strip().split(sep='/', maxsplit=1)
-    data.append((w, t))
-train_data = data[:-60000]
-held_out_data = data[-60000:-40000]
-test_data = data[-40000:]
+    # file = open('data/texten2.ptg', 'rt')
+    # data = []
+    # for line in file:
+    #     w, t = line.strip().split(sep='/', maxsplit=1)
+    #     data.append((w, t))
+    # train_data = data[:-60000]
+    # held_out_data = data[-60000:-40000]
+    # test_data = data[-40000:]
 
-# we will use only bigram distribution, otherwise the number of state would be much larger
-# get the words and the tags
-tag_set = list(set([t for w, t in train_data]))
-words = [w for w, t in train_data]
-word_counts = Counter(words)
-# words that are rarely in the test data are also unlikely to be in the testing data
-# but it significantly reduces the size of the matrix
-vocabulary = [w for w in word_counts if word_counts[w] > 5]
-# create dictionaries to convert word to index
-i = 1
-# if the word is not present, return 0 --- treat unknown words as a special OOV token
-# does not matter much, because those words are rare anyway
-word_to_index = defaultdict(lambda: 0)
-for word in vocabulary:
-    word_to_index[word] = 1
-    i += 1
-# tags are the states
-state_count = len(tag_set)
+    # we will use only bigram distribution, otherwise the number of state would be much larger
+    # get the words and the tags
+    tag_set = list(set([t for w, t in training_data]))
+    # make the starting state state number 0
+    tag_set.remove('###')
+    tag_set = ['###'] + tag_set
+    words = [w for w, t in training_data]
+    word_counts = Counter(words)
+    # words that are rarely in the test data are also unlikely to be in the testing data
+    # but it significantly reduces the size of the matrix
+    vocabulary = [w for w in word_counts if word_counts[w] > 5]
+    vocabulary.remove('###')
+    # make the starting state 0
+    vocabulary = ['###'] + vocabulary
+    # create dictionaries to convert word to index
+    # if the word is not present, return maximum --- treat unknown words as a special OOV token
+    # does not matter much, because those words are rare anyway
+    word_to_index = defaultdict(lambda: len(vocabulary))
+    for i in range(len(vocabulary)):
+        word_to_index[vocabulary[i]] = i
 
-# get P(w|t) and n-gram distributions, use only the first 10,000 words of T to estimate the initial (raw) parameters
-emission_distribution, dists = get_distributions(train_data[:10000])
-# get the smoothed transition distribution
-trans_distribution = Linear(held_out_data, dists[0], dists[1], dists[2])
+    # tags are the states
+    state_count = len(tag_set)
 
-# allocate the matrix
-transition_matrix = np.zeros((state_count, state_count))
-# fill in the transition matrix with the estimated probabilities
-for i in range(state_count):
-    for j in range(state_count):
-        transition_matrix[i, j] = trans_distribution.p(tag_set[i], tag_set[j])
+    # get P(w|t) and n-gram distributions, use only the first 10,000 words of T to estimate the initial (raw) parameters
+    emission_distribution, dists = get_distributions(train_data[:10000])
+    # get the smoothed transition distribution
+    trans_distribution = Linear(held_out_data, dists[0], dists[1], dists[2])
 
-emission_matrix = np.zeros((state_count, len(vocabulary)))
-for i in range(state_count):
-    for j in range(state_count):
-        transition_matrix[i, j] = trans_distribution.p(tag_set[i], tag_set[j])
+    # allocate the matrix
+    transition_matrix = np.zeros((state_count, state_count))
+    # fill in the transition matrix with the estimated probabilities
+    for i in range(state_count):
+        for j in range(state_count):
+            transition_matrix[i, j] = trans_distribution.p(tag_set[i], tag_set[j])
 
-convergence = False
-while not convergence:
-    alfa = compute_forward_probabilities(trans_distribution, output_distribution)
-    beta = compute_backward_probabilities(trans_distribution, output_distribution)
-    collect_counts()
-    reestimate()
-print('Result')
+    emission_matrix = np.zeros((state_count, len(vocabulary) + 1))
+    for i in range(state_count):
+        for j in range(len(vocabulary)):
+            # TODO : Smooth this distribution
+            emission_matrix[i, j] = emission_distribution.get((vocabulary[j], tag_set[i]), 0)
+
+    # preprocessed the data
+    data_T = preprocess_data(training_data, word_to_index)
+
+    epsilon = 0.001
+
+    convergence = False
+    while not convergence:
+        for training_sentence in data_T:
+            print('DEBUG: Processing a sentence')
+            alfa = compute_forward_probabilities(transition_matrix, emission_matrix, training_sentence, state_count)
+            beta = compute_backward_probabilities(transition_matrix, emission_matrix, training_sentence, state_count)
+            t_new, e_new = collect_counts_and_reestimate(alfa, beta)
+            # chceck convergence criterion
+            if np.linalg.norm(transition_matrix - t_new) < epsilon and np.linalg.norm(
+                            emission_matrix - e_new) < epsilon:
+                convergence = True
+            transition_matrix = t_new
+            emission_matrix = e_new
+    print('Result')
