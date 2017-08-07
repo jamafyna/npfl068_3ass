bcfd7bf1daf46042f1f4deec6ed1bfbb2dc4ade4
Vladan <vgloncak@gmail.com>
2017-08-07 02:52:34 +0200

    Finale Viterbiho

--- main.py
+++ main.py
@@ -34,13 +34,13 @@ parser.add_option("-k", "--known-states",
                   help="Use only the states from the training data, enforces smoothing")
 
 (options, args) = parser.parse_args()
-file_name = 'data/texten2.ptg'  # args[0]
+file_name = args[0]
 print('INFO: Processing the file "', file_name, '"')
 unk = not options.known
 if not unk:
     print('INFO: Limiting the sates of states to the states from the training data')
 lex = options.lex
-oov = True  # options.oov
+oov = options.oov
 supervised = options.supervised
 
 file = open(file_name, encoding="iso-8859-2", mode='rt')
